240a2120308dff2a0b4acf2362b1cd2ce2898d55
lonewolf2580 <linusngwee@yahoo.com>
2021-10-28 15:25:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ac9bol
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ac9bol)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import React from "react";
+import Greet from "component/Greet";
+import "./style.css";
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Greet />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
