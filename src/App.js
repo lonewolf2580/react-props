cb62d6b7948d3b1b73105b62bc6a523a0382ac0b
lonewolf2580 <linusngwee@yahoo.com>
2021-10-28 15:58:13 -0700

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import React from "react";
-import Greet from "component/Greet";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 import "./style.css";
 
 class App extends Component {
   render(){
     return (
       <div>
-        <Greet />
+        {/* Props in Functional Component */}
+        <Greet name="Bruce" heroName="Batman">
+          <p>Children Props</p>
+        </Greet>
+        <Greet name="Clark" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman">
+          <button>Action</button>
+        </Greet>
+
+        {/* Props in Class Component */}
+        <Welcome name="Bruce" heroName="Batman" />
+        <Welcome name="Clark" heroName="Superman" />
+        <Welcome name="Diana" heroName="Wonder Woman" />
+
       </div>
     );
   }
